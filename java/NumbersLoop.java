5a94b2c9bdf3b80d8e3745fabe33a76b8fea78c3
Kunal Bari <108876787+BariKunal@users.noreply.github.com>
2025-02-13 19:47:12 +0530

    Add files via upload

---
+++ java/NumbersLoop.java
@@ -0,0 +1,13 @@
+/*1. Print Numbers from 1 to 10
+Write a program to print the numbers from 1 to 10 using a for loop.*/
+
+public class NumbersLoop
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=10; i++)
+		{
+			System.out.print(" "+i);
+		}
+	}
+}
