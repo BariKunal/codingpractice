91448ec297ce1696c388b72db3dc98f2a831d168
Kunal Bari <108876787+BariKunal@users.noreply.github.com>
2025-02-21 10:47:05 +0530

    New Pattern progams

---
+++ java/p5.java
@@ -0,0 +1,30 @@
+
+/*
+	*
+	**
+	***
+	****
+	*****
+
+*/
+
+
+public class p5
+{
+	public static void main(String x[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			
+			for(int j=1; j<=5; j++)
+			{
+				if(i>=j)
+				{
+					System.out.print("*");
+					
+				}
+			}
+			System.out.println();
+		}
+	}
+}
